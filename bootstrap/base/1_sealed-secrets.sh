34b2e524c76b3d68acc5115e1f35b3837284274b
kaio-fellipe <kaio6fellipe@gmail.com>
2023-06-17 22:07:59 -0300

    Include Grafana Agent Traces config and sealed secrets deploy

---
+++ bootstrap/base/1_sealed-secrets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+helm repo update
+helm upgrade --install sealed-secrets sealed-secrets/sealed-secrets -n kube-system
+kubectl rollout status deployment sealed-secrets -n kube-system
+kubectl delete secret -n kube-system -l sealedsecrets.bitnami.com/sealed-secrets-key=active
+kubectl apply -f bootstrap/base/1_sealed-secrets.key || exit 1
+kubectl delete pod -n kube-system -l app.kubernetes.io/name=sealed-secrets
