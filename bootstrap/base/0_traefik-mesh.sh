7cb2f9833da6b20508b10adf53ae2e68d0179546
kaio-fellipe <kaio6fellipe@gmail.com>
2023-07-05 05:29:31 -0300

    Adjust typos and rebased files

--- bootstrap/base/0_traefik-mesh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace traefik-mesh
-helm repo add traefik https://traefik.github.io/charts
-helm repo update traefik
-helm upgrade --install traefik-mesh traefik/traefik-mesh --set kubedns=true --set logLevel=info --set controller.logLevel=info --set proxy.logLevel=info --set tracing.deploy=false --set tracing.jaeger.enabled=false --set metrics.deploy=false --set metrics.prometheus.enabled=false -n traefik-mesh
