249791ed8209025dcde6fa8dd8037fe4279c1347
kaio-fellipe <kaio6fellipe@gmail.com>
2023-07-27 12:57:21 -0300

    Split linkerd deployment and start the configuration of internal datasource for Grafana Cloud

--- bootstrap/base/1_linkerd.sh
+++ bootstrap/base/1_linkerd.sh
@@ -5,6 +5,7 @@ kubectl apply -f ./bootstrap/base/linkerd/crds --recursive
 kubectl create namespace linkerd || true
 kubectl apply -f ./bootstrap/base/linkerd/deployment --recursive
 linkerd check
+kubectl create namespace linkerd-viz || true
 kubectl apply -f ./bootstrap/base/linkerd/viz --recursive
 kubectl rollout status deployment web -n linkerd-viz
 kubectl rollout status deployment metrics-api -n linkerd-viz
